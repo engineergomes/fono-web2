e5fd663663d827a4389b061d256690a0ee27da54
Rafael Vasconcelos <rafaelvasconcelos@protonmail.com>
2024-05-16 18:22:18 -0300

    slate implemented

--- src/app/fonoaudiologa-juaragua-sul/slate/page.tsx
+++ src/app/fonoaudiologa-juaragua-sul/slate/page.tsx
@@ -1,75 +1,286 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 'use client';
 
-// Import React dependencies.
-import React, { useCallback, useState } from 'react';
-// Import the Slate editor factory.
-import { NodeEntry, createEditor } from 'slate';
+import React, { useCallback, useMemo } from 'react';
+import isHotkey from 'is-hotkey';
+import { Editable, withReact, useSlate, Slate } from 'slate-react';
+import { Editor, Transforms, createEditor, Descendant, Element as SlateElement } from 'slate';
+import { withHistory } from 'slate-history';
+import { Button, Icon, Toolbar } from '@/app/slateComponents';
+import {
+  Code,
+  ListBullets,
+  ListNumbers,
+  NumberOne,
+  NumberTwo,
+  Quotes,
+  TextAlignCenter,
+  TextAlignJustify,
+  TextAlignLeft,
+  TextAlignRight,
+  TextBolder,
+  TextItalic,
+  TextUnderline,
+} from 'phosphor-react-sc';
 
-// Import the Slate components and React plugin.
-import { Slate, Editable, withReact } from 'slate-react';
-import { BaseEditor, Descendant, Transforms, Element, Editor } from 'slate';
-import { ReactEditor } from 'slate-react';
-import { CodeElement } from '@/components/CodeElement';
-import { DefaultElement } from '@/components/DefaultElement';
+const HOTKEYS = {
+  'mod+b': 'bold',
+  'mod+i': 'italic',
+  'mod+u': 'underline',
+  'mod+`': 'code',
+};
 
-type CustomElement = { type: 'paragraph' | 'code'; children: CustomText[] };
-type CustomText = { text: string };
+type HotkeysType = {
+  'mod+b': 'bold';
+  'mod+i': 'italic';
+  'mod+u': 'underline';
+  'mod+`': 'code';
+};
 
-declare module 'slate' {
-  interface CustomTypes {
-    Editor: BaseEditor & ReactEditor;
-    Element: CustomElement;
-    Text: CustomText;
-  }
-}
+const LIST_TYPES = ['numbered-list', 'bulleted-list'];
+const TEXT_ALIGN_TYPES = ['left', 'center', 'right', 'justify'];
 
-const initialValue: CustomElement[] = [
-  {
-    type: 'paragraph',
-    children: [{ text: 'A line of text in a paragraph.' }],
-  },
-];
-
-const App = () => {
-  const [editor] = useState(() => withReact(createEditor()));
-  // Render the Slate context.
-
-  // Define a type for the props expected by CodeElement and DefaultElement
-  type ElementProps = {
-    attributes?: React.HTMLAttributes<HTMLElement>; // Adjusted to HTMLElement
-    children?: React.ReactNode;
-  };
-
-  // Use the ElementProps type in your renderElement function
-  const renderElement = useCallback((props: { element: { type: string } } & ElementProps) => {
-    switch (props.element.type) {
-      case 'code':
-        return <CodeElement {...props} />;
-      default:
-        return <DefaultElement {...props} />;
-    }
-  }, []);
+const RichTextExample = () => {
+  const renderElement = useCallback(
+    (props: React.JSX.IntrinsicAttributes & { attributes: any; children: any; element: any }) => <Element {...props} />,
+    []
+  );
+  const renderLeaf = useCallback(
+    (props: React.JSX.IntrinsicAttributes & { attributes: any; children: any; leaf: any }) => <Leaf {...props} />,
+    []
+  );
+  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
 
   return (
     <Slate editor={editor} initialValue={initialValue}>
+      <Toolbar className="flex justify-center items-center bg-white w-fit mx-auto">
+        <MarkButton format="bold" icon={<TextBolder size={25} />} />
+        <MarkButton format="italic" icon={<TextItalic size={25} />} />
+        <MarkButton format="underline" icon={<TextUnderline size={25} />} />
+        <MarkButton format="code" icon={<Code size={25} />} />
+        <BlockButton format="heading-one" icon={<NumberOne size={25} />} />
+        <BlockButton format="heading-two" icon={<NumberTwo size={25} />} />
+        <BlockButton format="block-quote" icon={<Quotes size={25} />} />
+        {/* <BlockButton format="numbered-list" icon={<ListNumbers size={25} />} /> */}
+        {/* <BlockButton format="bulleted-list" icon={<ListBullets size={25} />} /> */}
+        <BlockButton format="left" icon={<TextAlignLeft size={25} />} />
+        <BlockButton format="center" icon={<TextAlignCenter size={25} />} />
+        <BlockButton format="right" icon={<TextAlignRight size={25} />} />
+        <BlockButton format="justify" icon={<TextAlignJustify size={25} />} />
+      </Toolbar>
       <Editable
-        className="w-1/2 border border-black rounded mx-auto text-black min-h-[20vh] p-3"
         renderElement={renderElement}
+        renderLeaf={renderLeaf}
+        placeholder="Enter some rich text…"
+        spellCheck
+        autoFocus
         onKeyDown={(event) => {
-          if (event.key === '`' && event.ctrlKey) {
-            // Prevent the "`" from being inserted by default.
-            event.preventDefault();
-            // Otherwise, set the currently selected blocks type to "code".
-            Transforms.setNodes(
-              editor,
-              { type: 'code' },
-              { match: (n) => Element.isElement(n) && Editor.isBlock(editor, n) }
-            );
+          for (const hotkey in HOTKEYS) {
+            if (isHotkey(hotkey, event as any)) {
+              event.preventDefault();
+              const mark = HOTKEYS[hotkey as 'mod+b' | 'mod+i' | 'mod+u' | 'mod+`'];
+              toggleMark(editor, mark);
+            }
           }
         }}
+        className="text-black border border-black max-w-7xl mx-auto p-2 bg-white"
       />
     </Slate>
   );
 };
 
-export default App;
+const toggleBlock = (editor, format) => {
+  const isActive = isBlockActive(editor, format, TEXT_ALIGN_TYPES.includes(format) ? 'align' : 'type');
+  const isList = LIST_TYPES.includes(format);
+
+  Transforms.unwrapNodes(editor, {
+    match: (n) =>
+      !Editor.isEditor(n) &&
+      SlateElement.isElement(n) &&
+      LIST_TYPES.includes(n.type) &&
+      !TEXT_ALIGN_TYPES.includes(format),
+    split: true,
+  });
+  let newProperties: Partial<SlateElement>;
+  if (TEXT_ALIGN_TYPES.includes(format)) {
+    newProperties = {
+      align: isActive ? undefined : format,
+    };
+  } else {
+    newProperties = {
+      type: isActive ? 'paragraph' : isList ? 'list-item' : format,
+    };
+  }
+  Transforms.setNodes<SlateElement>(editor, newProperties);
+
+  if (!isActive && isList) {
+    const block = { type: format, children: [] };
+    Transforms.wrapNodes(editor, block);
+  }
+};
+
+const toggleMark = (editor, format) => {
+  const isActive = isMarkActive(editor, format);
+
+  if (isActive) {
+    Editor.removeMark(editor, format);
+  } else {
+    Editor.addMark(editor, format, true);
+  }
+};
+
+const isBlockActive = (editor, format, blockType = 'type') => {
+  const { selection } = editor;
+  if (!selection) return false;
+
+  const [match] = Array.from(
+    Editor.nodes(editor, {
+      at: Editor.unhangRange(editor, selection),
+      match: (n) => !Editor.isEditor(n) && SlateElement.isElement(n) && n[blockType] === format,
+    })
+  );
+
+  return !!match;
+};
+
+const isMarkActive = (editor, format) => {
+  const marks = Editor.marks(editor);
+  return marks ? marks[format] === true : false;
+};
+
+const Element = ({ attributes, children, element }) => {
+  const style = { textAlign: element.align };
+  switch (element.type) {
+    case 'block-quote':
+      return (
+        <blockquote style={style} {...attributes}>
+          {children}
+        </blockquote>
+      );
+    case 'bulleted-list':
+      return (
+        <ul style={style} {...attributes}>
+          {children}
+        </ul>
+      );
+    case 'heading-one':
+      return (
+        <h1 style={style} {...attributes}>
+          {children}
+        </h1>
+      );
+    case 'heading-two':
+      return (
+        <h2 style={style} {...attributes}>
+          {children}
+        </h2>
+      );
+    case 'list-item':
+      return (
+        <li style={style} {...attributes}>
+          {children}
+        </li>
+      );
+    case 'numbered-list':
+      return (
+        <ol style={style} {...attributes}>
+          {children}
+        </ol>
+      );
+    default:
+      return (
+        <p style={style} {...attributes}>
+          {children}
+        </p>
+      );
+  }
+};
+
+const Leaf = ({ attributes, children, leaf }) => {
+  if (leaf.bold) {
+    children = <strong>{children}</strong>;
+  }
+
+  if (leaf.code) {
+    children = <code>{children}</code>;
+  }
+
+  if (leaf.italic) {
+    children = <em>{children}</em>;
+  }
+
+  if (leaf.underline) {
+    children = <u>{children}</u>;
+  }
+
+  return <span {...attributes}>{children}</span>;
+};
+
+const BlockButton = ({ format, icon }) => {
+  const editor = useSlate();
+  return (
+    <Button
+      active={isBlockActive(editor, format, TEXT_ALIGN_TYPES.includes(format) ? 'align' : 'type')}
+      onMouseDown={(event) => {
+        event.preventDefault();
+        toggleBlock(editor, format);
+      }}
+    >
+      <Icon>{icon}</Icon>
+    </Button>
+  );
+};
+
+const MarkButton = ({ format, icon }) => {
+  const editor = useSlate();
+  return (
+    <Button
+      active={isMarkActive(editor, format)}
+      onMouseDown={(event) => {
+        event.preventDefault();
+        toggleMark(editor, format);
+      }}
+    >
+      <Icon>{icon}</Icon>
+    </Button>
+  );
+};
+
+const initialValue: Descendant[] = [
+  {
+    type: 'paragraph',
+    children: [
+      { text: 'This is editable ' },
+      { text: 'rich', bold: true },
+      { text: ' text, ' },
+      { text: 'much', italic: true },
+      { text: ' better than a ' },
+      { text: '<textarea>', code: true },
+      { text: '!' },
+    ],
+  },
+  {
+    type: 'paragraph',
+    children: [
+      {
+        text: "Since it's rich text, you can do things like turn a selection of text ",
+      },
+      { text: 'bold', bold: true },
+      {
+        text: ', or add a semantically rendered block quote in the middle of the page, like this:',
+      },
+    ],
+  },
+  {
+    type: 'block-quote',
+    children: [{ text: 'A wise quote.' }],
+  },
+  {
+    type: 'paragraph',
+    align: 'center',
+    children: [{ text: 'Try it out for yourself!' }],
+  },
+];
+
+export default RichTextExample;
