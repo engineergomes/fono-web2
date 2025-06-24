aebab69a9e38af4f8513926b3d143056ba9cc3bf
Rafael <eng.rsgomes@gmail.com>
2025-06-24 17:56:27 -0300

    wip

--- src/pages/fonoaudiologa-jaragua-sul.tsx
+++ src/pages/fonoaudiologa-jaragua-sul.tsx
@@ -5,7 +5,6 @@ import { OpenHours } from '@/components/OpenHours/OpenHours';
 import { Header } from '@/components/Header';
 import Footer from '@/components/Footer';
 import { Instagram } from '@/components/Instagram';
-import UrlProvider from '@/providers/UrlProvider';
 import Head from 'next/head';
 
 export default function Home() {
